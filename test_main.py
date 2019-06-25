438db9a25946ec41d84eaf92b4ecc4d6f8b9057c
Michael Murphy <murphym18@gmail.com>
2019-06-25 01:31:02 -0700

    fix broken test

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ class TestMain(unittest.TestCase):
             self.fail()
 
     def test_cross_product2(self):
-        expected = np.array([0.0, 0.0, 10.0])
+        expected = np.array([0.0, 0.0, 1.0])
         c = np.cross(list(main.a), list(main.b))
         np.testing.assert_allclose(expected, c)
 
