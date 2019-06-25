2b2459a5dba6a98559ebb70d647af92c60f2adc5
Michael Murphy <murphym18@gmail.com>
2019-06-25 02:34:26 -0700

    fix unit test

--- test_main.py
+++ test_main.py
@@ -7,10 +7,7 @@ class TestMain(unittest.TestCase):
     def test_cross_product(self):
         expected = np.array([0.0, 0.0, 1.0])
         c = np.cross(list(main.a), list(main.b))
-        try:
-            np.testing.assert_allclose(expected, c)
-        except AssertionError as e:
-            self.fail()
+        np.testing.assert_allclose(expected, c)
 
     def test_cross_product2(self):
         expected = np.array([0.0, 0.0, 1.0])
